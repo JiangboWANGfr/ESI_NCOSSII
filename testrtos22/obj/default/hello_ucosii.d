d9bf861f2c34c8e7621577aa7ae3f17ceae8c590
JiangboWANG <zzdr2533@gmail.com>
2023-12-13 12:16:46 +0100

    TEST

---
+++ testrtos22/obj/default/hello_ucosii.d
@@ -0,0 +1,46 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+ ../testrtos22_bsp//HAL/inc/includes.h \
+ ../testrtos22_bsp//HAL/inc/os_cpu.h \
+ ../testrtos22_bsp//HAL/inc/sys/alt_irq.h \
+ ../testrtos22_bsp//HAL/inc/nios2.h \
+ ../testrtos22_bsp//HAL/inc/alt_types.h ../testrtos22_bsp/system.h \
+ ../testrtos22_bsp/linker.h ../testrtos22_bsp//UCOSII/inc/os_cfg.h \
+ ../testrtos22_bsp//HAL/inc/sys/alt_alarm.h \
+ ../testrtos22_bsp//HAL/inc/sys/alt_llist.h \
+ ../testrtos22_bsp//HAL/inc/priv/alt_alarm.h \
+ ../testrtos22_bsp//UCOSII/inc/ucos_ii.h \
+ ../testrtos22_bsp//HAL/inc/os_cpu.h \
+ ../testrtos22_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../testrtos22_bsp//HAL/inc/io.h ../testrtos22_bsp//HAL/inc/alt_types.h
+
+../testrtos22_bsp//HAL/inc/includes.h:
+
+../testrtos22_bsp//HAL/inc/os_cpu.h:
+
+../testrtos22_bsp//HAL/inc/sys/alt_irq.h:
+
+../testrtos22_bsp//HAL/inc/nios2.h:
+
+../testrtos22_bsp//HAL/inc/alt_types.h:
+
+../testrtos22_bsp/system.h:
+
+../testrtos22_bsp/linker.h:
+
+../testrtos22_bsp//UCOSII/inc/os_cfg.h:
+
+../testrtos22_bsp//HAL/inc/sys/alt_alarm.h:
+
+../testrtos22_bsp//HAL/inc/sys/alt_llist.h:
+
+../testrtos22_bsp//HAL/inc/priv/alt_alarm.h:
+
+../testrtos22_bsp//UCOSII/inc/ucos_ii.h:
+
+../testrtos22_bsp//HAL/inc/os_cpu.h:
+
+../testrtos22_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../testrtos22_bsp//HAL/inc/io.h:
+
+../testrtos22_bsp//HAL/inc/alt_types.h:
